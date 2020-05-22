e93fb47b5448b1cdb9de2915a3e3d300cc526373
ankur-kesari <ankurkesari05@gmail.com.com>
2020-05-22 12:04:35 +0530

    program3

---
+++ day1/program3.py
@@ -0,0 +1,5 @@
+print("Enter number for swapping")
+num1=input("enter first number:")
+num2=input("enter second number:")
+num1,num2=num2,num1
+print("After swapping\nfirst number :",num1,"\nsecond number:",num2)
