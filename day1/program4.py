0a732dcee058978ae049a262617cb390e80e1d66
ankur-kesari <ankurkesari05@gmail.com.com>
2020-05-22 12:24:40 +0530

    program4

---
+++ day1/program4.py
@@ -0,0 +1,6 @@
+costprice=float(input("Enter cost price of item:"))
+sellingprice=float(input("Enter selling price:"))
+profit=sellingprice-costprice
+print("profit :",profit)
+pselling_price=sellingprice+(0.05*profit)
+print("selling price after 5% profit is :", pselling_price)
